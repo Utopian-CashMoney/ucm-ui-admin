b36045a7fae4a232c333a144f56b0f2c9b503f17
Josten Asercion <josten.asercion@smoothstack.com>
2021-06-02 21:20:40 -0400

    Now shows number of open accounts for each account type

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -9,18 +9,39 @@ import { HttpService } from 'src/app/shared/services/http.service';
 export class AccountsComponent implements OnInit {
   constructor(private httpService: HttpService) { }
   accounts: any;
+  userAccounts: any;
+  openUserAccountNum: any[][] = [];
   totalAccounts = 0;
 
   ngOnInit(): void {
     this.loadAllAccounts();
+    this.findNumberOfOpenAccounts();
   }
 
   loadAllAccounts() {
     this.httpService
-    .getAll('http://localhost:8080/api/accounts')
+      .getAll('http://localhost:8080/api/accounts')
       .subscribe((res) => {
         this.accounts = res;
         this.totalAccounts = this.accounts.length;
+
+      });
+  }
+
+  findNumberOfOpenAccounts() {
+    this.httpService
+      .getAll('http://localhost:8080/api/user_account')
+      .subscribe((res) => {
+        this.userAccounts = res;
+
+        this.accounts.forEach(a => {
+          var totalOpenAccounts = 0;
+          this.userAccounts.forEach(u => {
+            if (u.account.id == a.id)
+              totalOpenAccounts++;
+          })
+          this.openUserAccountNum.push([a.id, totalOpenAccounts]);
+        });
       });
   }
 }
