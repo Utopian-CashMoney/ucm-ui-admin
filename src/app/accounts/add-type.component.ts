562371ca76fac10ff849ff9e7108b4b872c60e2a
Josten Asercion <josten.asercion@smoothstack.com>
2021-11-19 14:09:06 -0500

    Hide irrelevant create account type info

--- src/app/accounts/add-type.component.ts
+++ src/app/accounts/add-type.component.ts
@@ -12,21 +12,23 @@ import { ConfirmDialogService } from '../confirmDialog/confirmDialogService.serv
 export class AddTypeComponent implements OnInit {
 
   form: FormGroup;
+  displayCredit = true;
   submitted = false;
+  inputCheck = false;
 
   constructor(private httpService: HttpService, private router: Router, private fb: FormBuilder,
     private ConfirmDialogService: ConfirmDialogService) {
-    
+
   }
 
   ngOnInit(): void {
     this.form = this.fb.group(
       {
-        type: [''],
+        type: [ 'CREDIT', Validators.required],
         name: ['', Validators.required],
+        perks: ['', [Validators.required]],
         creditLimit: ['', Validators.required],
         apr: ['', [Validators.required]],
-        perks: ['', [Validators.required]],
       },
     );
   }
@@ -35,44 +37,85 @@ export class AddTypeComponent implements OnInit {
     return this.form.controls;
   }
 
-  onReset(): void {
-    this.submitted = false;
-    this.form.reset();
+  showCredit() {
+    if (!this.displayCredit) {
+      this.displayCredit = true;
+      this.form.value.creditLimit = null;
+      this.form.value.apr = null;
+    }
   }
 
-onSubmit() {
-  
+  hideCredit() {
+    if (this.displayCredit) {
+      this.displayCredit = false;
+      this.form.value.creditLimit = 0;
+      this.form.value.apr = 0;
+    }
+    console.log(
+      `After hideCredit()
+      Type: ${this.form.value.type} 
+      Name: ${this.form.value.name}  
+      Credit Limit: ${this.form.value.creditLimit} 
+      APR: ${this.form.value.apr}  
+      Description: ${this.form.value.perks}`);
+  }
+
+  onReset(): void {
+    this.form.reset()
+    this.submitted = false;
+    this.inputCheck = false;
+    this.showCredit();
+    //this.form.controls["type"].setValue(2);
+  }
+
+  checkInput() {
+    this.inputCheck = true;
+    console.log(
+    `Type: ${this.form.value.type} 
+    Name: ${this.form.value.name}  
+    Credit Limit: ${this.form.value.creditLimit} 
+    APR: ${this.form.value.apr}  
+    Description: ${this.form.value.perks}`);
+  }
+
+  onSubmit() {
     this.submitted = true;
-    console.log("Type: " + this.form.value.type);
-    if(!this.form.errors && this.form.valid){
     this.httpService
-        .post('http://localhost:8081/api/accounts', this.form.value)
-        .subscribe((res) => {
-            this.router.navigate(['cashmoney/admin/accounts'])
-        });
+      .post('http://localhost:8081/api/accounts', this.form.value)
+      .subscribe((res) => {
+        this.router.navigate(['cashmoney/admin/accounts'])
+      });
+    window.location.reload();
+  }
+
+  confirmationString(): string {
+    const accountType = JSON.stringify(this.form.value.type).replace(/\"/g, "");
+    const accountName = JSON.stringify(this.form.value.name).replace(/\"/g, "");
+    const accountLimit = JSON.stringify(this.form.value.creditLimit).replace(/\"/g, "");
+    const accountAPR = JSON.stringify(this.form.value.apr).replace(/\"/g, "");
+    const accountPerks = JSON.stringify(this.form.value.perks).replace(/\"/g, "");
+
+    let message: string = 
+    `Type: ${accountType} 
+    Name: ${accountName}  
+    Credit Limit: ${accountLimit} 
+    APR: ${accountAPR}  
+    Description: ${accountPerks}`;
+
+    return message;
+  }
+
+  public openConfirmationDialog() {
+    if (!this.form.errors && this.form.valid) {
+      this.ConfirmDialogService.confirm('Confirm New Account Type' , this.confirmationString())
+        .then((confirmed) => {
+          if (confirmed) {
+            this.onSubmit();
+          }
+        })
+        .catch(() => console.log('User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)'));
     }
     else
-      console.log("Form has errors or is invalid.");
-
-}
-
-public openConfirmationDialog() {
-
-  const accountType = JSON.stringify(this.form.value.type).replace(/\"/g, "");  
-  const accountName = JSON.stringify(this.form.value.name).replace(/\"/g, "");
-  const accountLimit = JSON.stringify(this.form.value.creditLimit).replace(/\"/g, "");  
-  const accountAPR = JSON.stringify(this.form.value.apr).replace(/\"/g, "");  
-  const accountPerks = JSON.stringify(this.form.value.perks).replace(/\"/g, "");  
-
-
-  this.ConfirmDialogService.confirm('Please confirm...', 'Do you want to go ahead and create an account type with ' + ' type:"' + accountType +
-  '", name: "' + accountName + ', credit limit: $' + accountLimit + '", APR: ' + accountAPR + '%, ' + 'perks: ' + accountPerks + '?')      
-  .then((confirmed) => {
-       if (confirmed) {
-         this.onSubmit();
-       }
-     })
-     .catch(() => console.log('User dismissed the dialog (e.g., by using ESC, clicking the cross icon, or clicking outside the dialog)'));
- }
-
+      console.log("There are errors with the form");
+  }
 }
